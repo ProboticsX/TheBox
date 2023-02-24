f8df0b849d42b5b7ef90493b50a73a67e9f40185
ProboticsX <jainshubham0403@gmail.com>
2023-02-24 16:06:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/thebox/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.thebox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.thebox.R;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
